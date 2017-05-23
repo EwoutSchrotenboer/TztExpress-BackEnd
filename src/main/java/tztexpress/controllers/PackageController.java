f63ff96a473c115ed164ff683869f44c6a0b504c
sjoulbak <remco_meinen@hotmail.com>
2017-05-23 16:22:15 +0200

    WIP adding package crud example

---
+++ src/main/java/tztexpress/controllers/PackageController.java
@@ -0,0 +1,46 @@
+package tztexpress.controllers;
+
+import tztexpress.model.Package;
+import tztexpress.services.PackageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+public class PackageController {
+    private PackageService packageService;
+
+    @Autowired
+    public void setPackageService(PackageService packageService) {
+        this.packageService = packageService;
+    }
+
+    @RequestMapping(value = "/packages", method = RequestMethod.GET)
+    public @ResponseBody List<Package> listPackages(){
+        return packageService.listAll();
+    }
+
+    @RequestMapping(value = "/packages/{id}", method = RequestMethod.GET)
+    public @ResponseBody Package getPackage(@PathVariable String id){
+        return packageService.getById(Long.valueOf(id));
+    }
+
+    @RequestMapping(value = "/packages", method = RequestMethod.POST)
+    public @ResponseBody Package saveOrUpdatePackage(@RequestBody Package _package){
+        return packageService.saveOrUpdate(_package);
+    }
+
+    @RequestMapping(value = "/packages/{id}", method = RequestMethod.DELETE)
+    public String delete(@PathVariable String id){
+        packageService.delete(Long.valueOf(id));
+        return "Ok";
+    }
+}
