559b4706f494273602ce1d7093088d541dd02942
Ewout <ewout@ewout.io>
2017-06-04 16:40:35 +0200

    adjusted packagecontroller (auth + models), fixed bug in usercontroller

--- src/main/java/tztexpress/controllers/PackageController.java
+++ src/main/java/tztexpress/controllers/PackageController.java
@@ -1,13 +1,13 @@
 package tztexpress.controllers;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.*;
+import tztexpress.core.GenericResultHandler;
+import tztexpress.models.GenericResult;
 import tztexpress.models.Package;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import tztexpress.services.AuthenticationService;
 import tztexpress.services.PackageService;
 
 import java.util.List;
@@ -22,24 +22,34 @@ public class PackageController {
         this.packageService = packageService;
     }
 
+
     @RequestMapping(value = "/packages", method = RequestMethod.GET)
-    public @ResponseBody List<Package> listPackages(){
-        return packageService.listAll();
+    public @ResponseBody GenericResult<List<Package>> listPackages(@RequestHeader HttpHeaders headers)
+    {
+        if(AuthenticationService.ValidateToken(headers.getValuesAsList("Authentication"))) {
+            return GenericResultHandler.GenericResult(packageService.listAll());
+        }
+        else {
+            return GenericResultHandler.GenericExceptionResult("Invalid authentication token");
+        }
     }
 
-    @RequestMapping(value = "/packages/{id}", method = RequestMethod.GET)
-    public @ResponseBody Package getPackage(@PathVariable String id){
-        return packageService.getById(Long.valueOf(id));
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public @ResponseBody GenericResult<Package> getPackage(@RequestHeader HttpHeaders headers, @PathVariable String id) {
+        if(AuthenticationService.ValidateToken(headers.getValuesAsList("Authentication"))) {
+            return GenericResultHandler.GenericResult(packageService.getById(Long.valueOf(id)));
+        } else {
+            return GenericResultHandler.GenericExceptionResult("Invalid authentication token");
+        }
     }
 
-    @RequestMapping(value = "/packages", method = RequestMethod.POST)
-    public @ResponseBody Package saveOrUpdatePackage(@RequestBody Package _package){
-        return packageService.saveOrUpdate(_package);
-    }
-
-    @RequestMapping(value = "/packages/{id}", method = RequestMethod.DELETE)
-    public String delete(@PathVariable String id){
-        packageService.delete(Long.valueOf(id));
-        return "Ok";
-    }
+//    // TODO
+//    @RequestMapping(value = "/create", method = RequestMethod.POST)
+//    public @ResponseBody GenericResult<Package> createPackage(@RequestHeader HttpHeaders headers, @RequestBody PackageRequestModel packageRequestModel) {
+//        if(AuthenticationService.ValidateToken(headers.getValuesAsList("Authentication"))) {
+//            return null;
+//        } else {
+//            return GenericResultHandler.GenericExceptionResult("Invalid authentication token");
+//        }
+//    }
 }
