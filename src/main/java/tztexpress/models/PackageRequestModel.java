c4270efc49f981c15c74fa3e2195f7f4b74c9004
Ewout <ewout@ewout.io>
2017-06-04 21:01:02 +0200

    create package initial commit

--- src/main/java/tztexpress/models/PackageRequestModel.java
+++ src/main/java/tztexpress/models/PackageRequestModel.java
@@ -2,12 +2,9 @@ package tztexpress.models;
 
 import tztexpress.models.CourierChoiceModel;
 
-/**
- * Created by Ewout on 4-6-2017.
- */
 public class PackageRequestModel {
-    CourierChoiceModel CourierChoiceModel;
-    private Long weight;
-    private String value;
-    private String details;
+    public CourierChoiceModel CourierChoiceModel;
+    public Long weight;
+    public String value;
+    public String details;
 }
