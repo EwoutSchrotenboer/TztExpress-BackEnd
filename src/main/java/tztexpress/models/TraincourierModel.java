1a319915eaf228a3f184368f7d1fa030c7238fa4
Ewout <ewout@ewout.io>
2017-06-05 19:24:17 +0200

    expanded returnvalue for traincourier id request for backoffice

---
+++ src/main/java/tztexpress/models/TraincourierModel.java
@@ -0,0 +1,13 @@
+package tztexpress.models;
+
+/**
+ * TraincourierModel for Packages in backoffice application
+ */
+public class TraincourierModel {
+    public Long id;
+    public Boolean vogapproved;
+    public String identification;
+    public Long userid;
+    public String email;
+    public UserPackageModel user;
+}
