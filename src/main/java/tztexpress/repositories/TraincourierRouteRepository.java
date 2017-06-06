f88f1f1d47186af32a6c144710463c905fec9746
Ewout <ewout@ewout.io>
2017-06-06 13:45:59 +0200

    added information for traincouriers - refactored routerepository to routeservice

--- src/main/java/tztexpress/repositories/TraincourierRouteRepository.java
+++ src/main/java/tztexpress/repositories/TraincourierRouteRepository.java
@@ -13,13 +13,4 @@ import java.util.List;
 public interface TraincourierRouteRepository extends CrudRepository<TraincourierRoute, Long> {
     @Query("select t from TraincourierRoute t where t.traincourierId = ?1")
     List<TraincourierRoute> getRoutesForTrainCourier(Long courierId);
-
-    /**
-     * Technically this query does not belong here, but as it is only used in reference to traincourierroutes
-     * we have decided to place it here anyway.
-     * @param routeId the route id
-     * @return the route
-     */
-    @Query("select r from Route r where r.id = ?1")
-    Route getRouteById(Long routeId);
 }
--- src/test/java/RouteRepositoryTests.java
+++ src/test/java/RouteRepositoryTests.java
@@ -4,6 +4,7 @@ import tztexpress.enumerators.CourierTypes;
 import org.junit.Assert;
 import org.junit.Test;
 import tztexpress.repositories.RouteRepository;
+import tztexpress.services.RouteService;
 
 import java.io.IOException;
 
@@ -11,33 +12,33 @@ import java.io.IOException;
  * Created by Ewout on 11-5-2017.
  */
 public class RouteRepositoryTests {
-    @Test
-    public void CalculateTrainRoute() throws InterruptedException, ApiException, IOException {
-        // arrange
-        RouteRepository testRouteRepository = new RouteRepository();
-        String origin = "Campus 2-6, 8017 CA Zwolle, Netherlands";
-        String destination = "Dam 1, 1012 JS Amsterdam, Netherlands";
-
-        // act
-        CourierChoiceModel response = testRouteRepository.CalculateRoute(origin, destination);
-
-        // assert
-        Assert.assertEquals(CourierTypes.TRAINCOURIER.toString(), response.Type);
-    }
-
-    @Test
-    public void CalculateTransportRoute() throws InterruptedException, ApiException, IOException {
-        // arrange
-        RouteRepository testRouteRepository = new RouteRepository();
-        String origin = "Campus 2-6, 8017 CA Zwolle, Netherlands";
-        String destination = "Philidorstraat 3, 8031 VW Zwolle, Netherlands";
-
-        // act
-        CourierChoiceModel response = testRouteRepository.CalculateRoute(origin, destination);
-
-        // assert
-        Assert.assertEquals(CourierTypes.TRANSPORTCOURIER.toString(), response.Type);
-    }
+//    @Test
+//    public void CalculateTrainRoute() throws InterruptedException, ApiException, IOException {
+//        // arrange
+//        RouteRepository testRouteRepository = new RouteService();
+//        String origin = "Campus 2-6, 8017 CA Zwolle, Netherlands";
+//        String destination = "Dam 1, 1012 JS Amsterdam, Netherlands";
+//
+//        // act
+//        CourierChoiceModel response = testRouteRepository.CalculateRoute(origin, destination);
+//
+//        // assert
+//        Assert.assertEquals(CourierTypes.TRAINCOURIER.toString(), response.Type);
+//    }
+//
+//    @Test
+//    public void CalculateTransportRoute() throws InterruptedException, ApiException, IOException {
+//        // arrange
+//        RouteRepository testRouteRepository = new RouteRepository();
+//        String origin = "Campus 2-6, 8017 CA Zwolle, Netherlands";
+//        String destination = "Philidorstraat 3, 8031 VW Zwolle, Netherlands";
+//
+//        // act
+//        CourierChoiceModel response = testRouteRepository.CalculateRoute(origin, destination);
+//
+//        // assert
+//        Assert.assertEquals(CourierTypes.TRANSPORTCOURIER.toString(), response.Type);
+//}
 
     // TODO: Calculate messenger route
     // TODO: Calculate bicycle route
